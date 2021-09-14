29d3c2fba590bb5582a7a854730ad1a398319e6f
John H <email@johnhatvani.com>
2021-09-14 18:29:06 +1000

    chore(project): initial xcode project setup

---
+++ VoiceMemo/VoiceMemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VoiceMemo
+//
+//  Created by John Hatvani on 14/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
